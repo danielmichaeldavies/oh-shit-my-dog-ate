9ebfac1b10c174cbb207ffdc812a3ba4135254df
Daniel Davies <daniel.davies@dubitlimited.com>
2019-12-12 08:04:18 +0000

    :lipstick: Begin construction of a basic app - header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1 className="app-title">Oh Shit! My Dog Ate...</h1>
+        <img src={logo} className="app-logo" alt="logo" />
       </header>
     </div>
   );
