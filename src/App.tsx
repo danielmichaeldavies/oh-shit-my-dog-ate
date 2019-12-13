19fc5eb50b57e9a1e17e2c5dd61f873df5727623
Daniel Davies <daniel.davies@dubitlimited.com>
2019-12-13 19:26:53 +0000

    :sparkles: Add a text input that sets state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 
 const App: React.FC = () => {
+  const [currentArticle, setCurrentArticle] = useState('');
+  const handleInputChange = ({ target: { value } }: { target: { value: string } }) => {
+    setCurrentArticle(value);
+  };
+
   return (
     <div className="app">
       <header>
         <h1 className="app-title">Oh Shit! My Dog Ate...</h1>
         <img src={logo} className="app-logo" alt="logo" />
       </header>
+
+      <main>
+        <input className="article-input"
+               placeholder="What did they eat?"
+               onChange={handleInputChange}
+               value={currentArticle} />
+        <p>{currentArticle}</p>
+      </main>
     </div>
   );
 }
